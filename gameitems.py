c949c4ae70da86c66b33fffc14d1e35a7029c821
Sergey <sergey1klyuev@gmail.com>
2016-12-26 15:04:53 +0300

    Added user logic

--- gameitems.py
+++ gameitems.py
@@ -8,7 +8,7 @@ class Bag(object):
 
     def __init__(self):
         """Fill a bag with kegs"""
-        self.bag = [i for i in range(MIN_KEG, MAX_KEG + 1)]
+        self.bag = [str(i) for i in range(MIN_KEG, MAX_KEG + 1)]
 
     def get_keg(self):
         """Return a random keg from the bag"""
@@ -30,7 +30,7 @@ class Card(object):
     def __init__(self, name):
         """Initialize a new card"""
         self.name = name
-        self.pool = [i for i in range(MIN_KEG, MAX_KEG + 1)]
+        self.pool = [str(i) for i in range(MIN_KEG, MAX_KEG + 1)]
         self.card = [self._get_line() for i in range(self.LINE_COUNT)]
 
     def _get_line(self):
@@ -49,6 +49,9 @@ class Card(object):
         return random.sample(range(self.LINE_LEN), self.EMPTY_CELLS)
 
     def _get_number(self):
+        """Return a random number from the pool or
+                  'None' if the pool is empty.
+        """
         try:
             selected_number = random.choice(self.pool)
             self.pool.remove(selected_number)
@@ -57,14 +60,42 @@ class Card(object):
             print "Pool is empty! -> {}".format(self.pool)
             return None
 
-    def cross_number(self):
-        pass
+    def check(self, keg):
+        """Return tuple: (line number of the card, keg index) or
+                  'None' if keg value not found.
+        """
+        for i, line in enumerate(self.card):
+            if keg in line:
+                ikeg = line.index(keg)
+                return (i, ikeg)
+        return None
 
-    def do_nothing(self):
-        pass
+    def cross(self, ikeg, index):
+        """Cross keg value"""
+        try:
+            self.card[index][ikeg] = '-'
+        except (IndexError, ValueError):
+            print "Can't cross a keg {} on line {}".format(ikeg, index)
+            print self.card
+
+    def check_win_condition(self):
+        """Return line number if all kegs for a line is crossed otherwise
+           return 0"""
+        for index, line in enumerate(self.card):
+            if self._check_line(line):
+                return index + 1
+        return 0
+
+    def _check_line(self, line):
+        """Return 1 if all kegs are crossed otherwise
+            return 0"""
+        for item in line:
+            if item.isdigit():
+                return 0
+        return 1
 
     def __str__(self):
-        '''Print a card'''
+        """Print a card"""
         card = '-'*10 + self.name + '-'*9 + '\n'
         for line in self.card:
             card += ' '.join(['{:2}'.format(item) for item in line])
--- main.py
+++ main.py
@@ -1,18 +1,47 @@
-import random
 import gameitems
+
+def handle_response(resp):
+    """Return 1 for  positive response or
+              0 for negative"""
+    while True:
+        if resp in ["y", "yes"]:
+            return 1
+        elif resp in ["n", "no"]:
+            return 0
+        else:
+            print "Input:{%s} is invalid! Try again!"
+            resp = raw_input()
 
 if __name__ == '__main__':
     bag = gameitems.Bag()
     my_card = gameitems.Card("My card")
-    pc_card = gameitems.Card("PC card")
 
     print my_card
-    print pc_card
-    
+
+    count = 1
     while True:
-        keg = bag.get_keg()
+        keg = bag.get_keg()  # draw a new keg
         if keg:
             print "New number: {}".format(keg)
+            check_result = my_card.check(keg)
+            if check_result:
+                line_index, keg_index = check_result
+                print "Do you want to cross the number {} on line {}.\n" \
+                      "(y/n)?".format(keg, line_index + 1)
+                response = raw_input().lower()
+                if handle_response(response):
+                    my_card.cross(keg_index, line_index)
+                    winner_line = my_card.check_win_condition()
+                    if winner_line:
+                        print "!!!  " + "WIN" + " !!!"
+                        print "All kegs are corssed on line: \
+                              {}".format(winner_line)
+                        print my_card
+                        print "Game duration: {} turns".format(count)
+                        break
+                    print my_card
+            count += 1
         else:
+            # Bag is empty
             print 'Game over!'
             break
